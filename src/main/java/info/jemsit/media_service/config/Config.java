ed989b7fded607b5879b34dc813d177f046ebd88
jimmy <jimishukurow@gmail.com>
2026-02-26 18:06:58 +0500

    fixed minio config

--- src/main/java/info/jemsit/media_service/config/Config.java
+++ src/main/java/info/jemsit/media_service/config/Config.java
@@ -1,17 +1,27 @@
 package info.jemsit.media_service.config;
 
 import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
 
+    @Value("${minio.url}")
+    private String url;
+
+    @Value("${minio.accessKey}")
+    private String accessKey;
+
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
     @Bean
     public MinioClient minioClient() {
         return MinioClient.builder()
-                .endpoint("http://localhost:9000")
-                .credentials("ROOTNAME", "CHANGEME123")
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
                 .build();
     }
 }
