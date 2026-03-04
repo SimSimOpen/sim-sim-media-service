31b10f6f3ec31fb14b8eaf3b6126578ff41201ac
jimmy <jimishukurow@gmail.com>
2026-03-04 15:03:26 +0500

    creating queues for exchange, before it startes, believe me it is needed

--- src/main/java/info/jemsit/media_service/config/RabbitMQConfig.java
+++ src/main/java/info/jemsit/media_service/config/RabbitMQConfig.java
@@ -1,16 +1,22 @@
 package info.jemsit.media_service.config;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static info.jemsit.common.data.constants.RabbitMQConstants.MEDIA_QUEUE;
 
 
 @Configuration
 public class RabbitMQConfig {
 
+    @Bean
+    public Queue eventQueue() {
+        return new Queue(MEDIA_QUEUE, true);
+    }
 
     @Bean
     public Jackson2JsonMessageConverter messageConverter(){
